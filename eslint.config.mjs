bfebe846ee239ef8c9fe58ea1c7b561c658d47fe
carlos.simon <caloycamacho02@gmail.com>
2025-01-10 17:53:35 +0800

    chore: install and configure Husky, ESLint and Prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/recommended",
+    "prettier"
+  ),
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+  },
+];
+
+export default eslintConfig;
